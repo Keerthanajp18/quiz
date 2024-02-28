1141235fdfa486a4a3b626f8a4534914cb0f7f6b
fathimafemin <160300994+fathimafemin@users.noreply.github.com>
2024-02-28 17:27:35 +0530

    initial commit

---
+++ lib/question.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/questionlist.dart';
+
+class que extends StatefulWidget {
+  const que({Key? key}) : super(key: key);
+
+  @override
+  State<que> createState() => _queState();
+}
+
+class _queState extends State<que> {
+  List question=[
+    Quiz(qus:'linnet is single',ans:true),
+    Quiz(qus:'diya is a ammayi',ans:true),
+    Quiz(qus:'hamna have nice voice',ans:false),
+    Quiz(qus:'linnet is a pottathi',ans:true),
+    Quiz(qus:'sreya sings very well',ans:false),
+    Quiz(qus:'lakshmi is super',ans:false),
+    Quiz(qus:'keerthana is a nice girl',ans:true),
+    Quiz(qus:'shamil is sugu',ans:true),
+    Quiz(qus:'sana have blood fear',ans:true),
+    Quiz(qus:'sinan is a singer',ans:true),
+  ];
+
+  int q_no=0;
+  void nextqns(){
+
+    if(q_no<question.length){
+      q_no++;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellowAccent,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 200,
+            ),
+            Text(
+              question[q_no].qus,
+              style: TextStyle(color: Colors.black, fontSize: 30),
+            ),
+            SizedBox(
+              height: 200,
+            ),
+            Container(
+              height: 60,
+              width: 300,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.green),
+                child: Text(
+                  'TRUE',
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  setState((){
+                    nextqns();
+                  });
+                  print('super like keerthnh');
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 60,
+              width: 300,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.red),
+                child: Text(
+                  'FALSE',
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  setState((){
+                    nextqns();
+                  });
+                  print('potttan like shamil');
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '✔ ✔ ✖ ✔ ✖',
+              style: TextStyle(fontSize: 30),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
